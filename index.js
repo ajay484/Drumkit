119677ba4c0fa0150440f4d495c787dbccccf22a
ajay484 <118597536+ajay484@users.noreply.github.com>
2023-08-08 23:26:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+var numberofbuttons=document.querySelectorAll(".drum").length;
+
+for(var i=0;i<numberofbuttons;i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+    var buttonInnerHTML=this.innerHTML;
+    makeSound(buttonInnerHTML);
+
+  });
+}
+
+document.addEventListener("keypress", function(event){
+  makeSound(event.key);
+});
+
+
+function makeSound(key){
+  switch(key){
+  case "W":
+      var tom1 = new Audio("./sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "A":
+      var tom2 = new Audio("./sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "S":
+      var tom3 = new Audio("./sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "D":
+      var tom4 = new Audio("./sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case "J":
+      var snare = new Audio("./sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "K":
+      var crash = new Audio("./sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "L":
+      var kick = new Audio("./sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+
+    default: console.log(key);
+
+  }
+}
